43c0bd7f94c8f7d80f7f03a9342fb73019580e1a
Seif-Alamir Yousef <seifalamir.yousef@gmail.com>
2024-03-05 17:38:44 +0100

    ..

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     socket.on('occupancyUpdated', (data) => {
         console.log('Occupancy updated: ', data);
         // Assuming you have elements with IDs corresponding to these values
-        document.getElementById('entryCount').textContent = `Entry: ${data.entry}`;
+        document.getElementById('entryCount').textContent = `Total Count: ${data.entry}`;
         document.getElementById('groundFloorCount').textContent = `Ground Floor: ${data.groundFloor}`;
         document.getElementById('firstFloorCount').textContent = `First Floor: ${data.firstFloor}`;
         document.getElementById('secondFloorCount').textContent = `Second Floor: ${data.secondFloor}`;
